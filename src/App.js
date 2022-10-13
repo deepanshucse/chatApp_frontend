ab5ee0be9923af5dd5575f8257379145c0611826
Deepanshu Yadav <deepanshu.yadav@qsstechnosoft.com>
2022-10-14 00:05:48 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
+import axios from "axios";
 
 function App() {
+  axios.defaults.baseURL = 'https://chatappdeep.herokuapp.com';
   return (
     <div className="App">
       <Route path="/" component={HomePage} exact />
